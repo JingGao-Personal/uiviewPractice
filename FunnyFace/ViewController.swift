373120da71598d670bc4d4fd3e14844ad234d9fe
Jing Gao <tracygj1990@gmail.com>
2017-08-18 14:20:05 +1000

    Initial Commit

---
+++ FunnyFace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunnyFace
+//
+//  Created by Jing Gao on 18/8/17.
+//  Copyright © 2017 Jing Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

3c8eaa24edc40ff9a502dc606a98d8d7c573ff77
Jing Gao <tracygj1990@gmail.com>
2017-08-28 21:52:15 +1000

    small push

---
+++ FunnyFace/EmotionsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  EmotionsViewController.swift
+//  FunnyFace
+//
+//  Created by Jing Gao on 28/8/17.
+//  Copyright © 2017 Jing Gao. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+    
+    
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
